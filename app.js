068b0d42e1b045c325c4c5aaff2fe11c425837b4
David Middleton <middleton.davida@gmail.com>
2020-05-13 16:26:09 -0400

    added form open and close functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 class App {
   constructor() {
-
+    this.$form = document.querySelector('#form');
+    this.$noteTitle = document.querySelector('#note-title');
+    this.$formButtons = document.querySelector('#form-buttons')
     this.addEventListeners();
   }
 
@@ -8,6 +10,27 @@ class App {
     document.body.addEventListener('click', event => {
       this.handleFormClick(event);
     });
-  
+  }
 
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeform();
+    }
+  }
+
+  openForm() {
+    this.$form.classList.add('form-open');
+    this.$noteTitle.style.display = 'block';
+    this.$formButtons.style.display = 'block';
+  }
+
+  closeform() {
+    this.$form.classList.remove('form-open');
+    this.$noteTitle.style.display = 'none';
+    this.$formButtons.style.display = 'none';
+  }
+}
 new App()
