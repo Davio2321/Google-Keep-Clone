a01719a16bb5e89dba5a8c473c70b38510925d2b
David Middleton <middleton.davida@gmail.com>
2020-05-13 17:21:31 -0400

    added notes to constructor, doc.body click event, and handleFormClick

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 class App {
   constructor() {
+    // following are references to html elements, noted by $
     this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('#note-title');
     this.$formButtons = document.querySelector('#form-buttons')
-    this.addEventListeners();
+    this.addEventListeners(); /*Calls the addEventListener function when the app opens*/
   }
 
   addEventListeners() {
-    document.body.addEventListener('click', event => {
+    document.body.addEventListener('click', event => { /*add click events to document body*/
       this.handleFormClick(event);
     });
+
+    this.$form.addEventListener('submit', event =>{
+      this.handleSubmit(event);
+    })
+
   }
 
-  handleFormClick(event) {
+  handleFormClick(event) { /*Will open the form if the click event contains the form or close if outside */
     const isFormClicked = this.$form.contains(event.target);
     if (isFormClicked) {
       this.openForm();
@@ -31,6 +37,11 @@ class App {
     this.$form.classList.remove('form-open');
     this.$noteTitle.style.display = 'none';
     this.$formButtons.style.display = 'none';
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
   }
 }
 new App()
